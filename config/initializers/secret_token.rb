c455a5ffb6398345e3a665017918a910a284fa31
afansky <afansky@gmail.com>
2012-12-16 21:21:32 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Historytracker::Application.config.secret_token = '68627d1353023b644dc078271cd33bb4a3e321a874072010adccd6471d208833819423ba88557090a1afb1395acfd4f559dec023b2d499aff0747550d045cf3d'
